61866b61199e56001a8f85425715dfb033fae2d8
Jakiati11 <91896925+Jakiati11@users.noreply.github.com>
2022-03-05 10:57:18 +0700

    Add files via upload

---
+++ praktikum01/variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+// definisikan variables
+$nama ='Jakiati Rifqoh Assalwa Koswara';
+$umur = 20;
+$berat = 57;
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+echo "<br/>Hello $nama Apa kabar?";
+?>
